4e815f8be79458e477e41f773edab32daa143da4
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-07 20:51:25 -0700

    Created aboutRouter

---
+++ routes/aboutRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const aboutRouter = express.Router();
+aboutRouter.use(bodyParser.json());
+
+    aboutRouter.route('/')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res) => {
+        res.end('Will send the about information to you')
+    })
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /abouts');
+    })
+
+module.exports = aboutRouter;
