36b98e45a6f6a3bd5c991b2e7b0b3881f3a5d325
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-07 20:52:55 -0700

    Created contactRouter

---
+++ routes/contactRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const contactRouter = express.Router();
+contactRouter.use(bodyParser.json());
+
+    contactRouter.route('/')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res) => {
+        res.end('Will send the contact information to you')
+    })
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /contacts');
+    })
+
+module.exports = contactRouter;
